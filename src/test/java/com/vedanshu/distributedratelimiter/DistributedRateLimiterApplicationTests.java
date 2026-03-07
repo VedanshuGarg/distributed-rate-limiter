ab7ce95735413ca68af51fbf95eea88a86fd1f13
Vedanshu Garg <garg.vedanshu04@gmail.com>
2026-03-07 15:56:13 +0530

    chore: initial spring boot setup

---
+++ src/test/java/com/vedanshu/distributedratelimiter/DistributedRateLimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vedanshu.distributedratelimiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedRateLimiterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
